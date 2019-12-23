fc5d8d6d9d4d8a7f6609122d8d7b9aee6f393dce
achsofiene10 <51764476+achsofiene10@users.noreply.github.com>
2019-12-23 23:51:19 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('favs')->default('');
+            $table->text('favs')->default('[]');
             $table->rememberToken();
             $table->timestamps();
         });
